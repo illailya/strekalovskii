75fb87f1f481b5291967f1ba2258d314a5760249
illailya <Stril1991@mail.ru>
2025-05-20 17:48:05 +0300

    Задача 1

--- lesson3.py
+++ lesson3.py
@@ -12,3 +12,13 @@ result1 = numbers[1:5]
 result2 = numbers[::2]
 result3 = numbers[::-1]
 print(f"Извлечь элементы со 2-го по 5-й (включительно): {result1}, \nИзвлечь каждый второй элемент списка: {result2}, \nИзвлечь список в обратном порядке: {result3}")
+
+#Задача 3
+
+languages = ['Python', 'Java', 'C++', 'JavaScript', 'Ruby', 'PHP']
+del languages[2]
+print(f"Удалить 'C++' по значению: {languages}")
+del languages[3]
+print(f"Удалить элемент с индексом 3: {languages}")
+languages.clear()
+print(f"Очистить список полностью: {languages}")
