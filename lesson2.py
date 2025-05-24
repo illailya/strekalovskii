094c52c63619eaedb1f703e1388effab3a1d1f4a
illailya <Stril1991@mail.ru>
2025-05-24 17:16:40 +0300

    Задача 1

--- lesson2.py
+++ lesson2.py
@@ -168,3 +168,28 @@ movies = ['брат', 'брат 2', 'брат 3']
 movies.append("форсаж")
 movies[1] = 'Титаник'
 print(f"{movies}\nДлина списка: {len(movies)}")
+
+#задача 18
+
+fruits = ["яблоко", "банан"]
+fruits.append("апельсин")
+fruits.insert(0, "киви")
+print(f"Итоговый список: {fruits}")
+
+#Задача 19
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+result1 = numbers[1:5]
+result2 = numbers[::2]
+result3 = numbers[::-1]
+print(f"Извлечь элементы со 2-го по 5-й (включительно): {result1}, \nИзвлечь каждый второй элемент списка: {result2}, \nИзвлечь список в обратном порядке: {result3}")
+
+#Задача 20
+
+languages = ['Python', 'Java', 'C++', 'JavaScript', 'Ruby', 'PHP']
+del languages[2]
+print(f"Удалить 'C++' по значению: {languages}")
+del languages[3]
+print(f"Удалить элемент с индексом 3: {languages}")
+languages.clear()
+print(f"Очистить список полностью: {languages}")
--- lesson3.py
+++ lesson3.py
@@ -1,24 +1,7 @@
-#задача 1
+#Условные операторы
 
-fruits = ["яблоко", "банан"]
-fruits.append("апельсин")
-fruits.insert(0, "киви")
-print(f"Итоговый список: {fruits}")
+#Задача 1
 
-#Задача 2
-
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-result1 = numbers[1:5]
-result2 = numbers[::2]
-result3 = numbers[::-1]
-print(f"Извлечь элементы со 2-го по 5-й (включительно): {result1}, \nИзвлечь каждый второй элемент списка: {result2}, \nИзвлечь список в обратном порядке: {result3}")
-
-#Задача 3
-
-languages = ['Python', 'Java', 'C++', 'JavaScript', 'Ruby', 'PHP']
-del languages[2]
-print(f"Удалить 'C++' по значению: {languages}")
-del languages[3]
-print(f"Удалить элемент с индексом 3: {languages}")
-languages.clear()
-print(f"Очистить список полностью: {languages}")
+x = 1
+if x > 0:
+    print('Число положительное')
