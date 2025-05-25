00bfaf2271205140cb3cb6cb1afffcb6df927af9
illailya <Stril1991@mail.ru>
2025-05-25 16:02:28 +0300

    Задача 1

--- lesson5.py
+++ lesson5.py
@@ -55,3 +55,45 @@ keys_list = country.keys()
 values_list = country.values()
 has_tokyo = "Токио" in values_list
 print(f"Ключи: {keys_list}\nЗначения: {values_list} Значение 'Токио' присутствует: {has_tokyo}")
+
+#Задача 5
+
+fruit = {
+"name": "яблоко",
+"color": "зелёное",
+"price": 50
+}
+print(fruit)
+
+#задача 6
+
+book = {
+    "title": "Гарри Поттер",
+    "author": "Дж. Роулинг",
+    "year": 1997
+}
+print(book["author"])
+
+#Задача 7
+
+phone = {
+    "brand": "Samsung",
+    "model": "Galaxy S21"
+}
+phone["year"] = 2021
+print(phone)
+
+#Задача 8
+
+user = {
+    "login": "super_user",
+    "email": "user@mail.com",
+    "age": 25,
+    "is_verified": False
+}
+age = user.pop("age")
+print(age)
+if "is_verified" in user:
+    print(user["is_verified"])
+else:
+    print("Ключ отсутствует")
