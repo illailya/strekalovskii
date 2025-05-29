074710bc417261eb178523242e58ce48a5f65a17
illailya <Stril1991@mail.ru>
2025-05-29 22:58:21 +0300

    Задача 1

--- lesson6.py
+++ lesson6.py
@@ -23,4 +23,25 @@ while i <= 5:
 for num in range(3,9):
     print(num)
 
+#Задача 5
 
+for num in range(0,5):
+    print(num)
+
+#Задача 6
+
+for text in "код":
+    print(text)
+
+#Задача 7
+
+num = 5
+while num > 0:
+    print(num)
+    num -=1
+
+#задача 8
+
+temps = [25.3, 18.7, 30.5, 22.0, 17.1]
+for temp in temps:
+    print(f"Температура: {temp}")
