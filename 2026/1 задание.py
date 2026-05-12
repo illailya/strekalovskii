3d4a6022f68278951b07cb3d66773f60ce8be329
illailya <Stril1991@mail.ru>
2026-05-12 13:28:13 +0300

    проба пера

--- 2026/1 задание.py
+++ 2026/1 задание.py
@@ -1,2 +1,16 @@
-age = 25
-print(age)
+# age = 25
+# print(age)
+
+# name = "ilya"
+# age = 25
+# is_active = True
+# print(name)
+# print(age)
+# print(is_active)
+
+# number_str = "42"
+# num_int = int(number_str)
+# num_float = float(num_int)
+# print(type(number_str))
+# print(type(num_int))
+# print(type(num_float))
